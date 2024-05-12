baddcf6ebaaad4520e5a007a1efbb5ef6c768048
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-05-12 14:23:36 -0600

    Se implemento la conexion a base de datos

---
+++ configs/server.js
@@ -0,0 +1,39 @@
+
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js'
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.connectDB();
+    this.routes();
+    this.port = process.env.PORT;
+  }
+
+  async connectDB() {
+    await dbConnection()
+  }
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use(helmet());
+    this.app.use(morgan('dev'));
+  }
+
+  routes() { }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Server running on port', this.port);
+    });
+  }
+}
+
+export default Server;
